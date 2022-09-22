71247fc5a5661d5439f0a7117a119bd001c26a4d
Abhishek Arya <abhishek13arya@gmail.com>
2022-09-22 22:25:19 +0530

    entry-app

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'hero-pattern': "url('./home.jpg')",
+        'hero-pattern': "url('/home.jpg')",
       }
     },
   },
